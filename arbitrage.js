76be9c75bcdafc6cab2f4c10665b04b4c140573b
ninjapretzel <ninjapretzel@gmail.com>
2019-12-03 10:56:04 -0700

    Makes the JS one much, much faster by using a custom queue instead of pop/unshift.

--- arbitrage.js
+++ arbitrage.js
@@ -2,6 +2,19 @@
 // File system libray
 const fs = require("fs");
 
+// Helper class for a performant queue
+// I tried switching pop/unshift to shift/push, and it still behaved very poorly,
+// So instead I figured I'd make a little queue that mimiced what I thought the go version was doing
+// Turns out that this is really, really, really fast.
+function Queue() {
+	this.stuff = []
+	this.added = 0
+	this.removed = 0;
+	this.put = (thing )=>{ this.stuff[this.added++] = thing; }
+	this.take = ()=>{ let it = this.stuff[this.removed]; this.stuff[this.removed++] = undefined; return it; }
+	this.count = ()=>{ return this.added - this.removed; }
+}
+
 // Load the file as a text file
 const text = fs.readFileSync("stuff.dat", 'utf-8');
 // console.log(text);
@@ -16,7 +29,7 @@ let lines = text.split('\n')
 //		graph["Dollar"]["Canada"] will hold conversion rate for US$ to CND$
 let graph = {}
 // work queue, holds stuff we still need to do
-let queue = []
+let queue = new Queue()
 // List of profitable finished paths.
 // We don't need to care about unprofitable paths
 // Unprofitable paths are just the reverse of profitable ones
@@ -60,32 +73,6 @@ for (let i = 0; i < lines.length; i++) {
 
 // console.log(graph);
 
-/* 
-// Javascript is really slow and can't keep up with 9 of these for some reason.
-Object.keys(graph).forEach((k) => {
-	queue.push( { 
-		start: k, // Remember where we start
-		at: k, // current location is where we are starting initially
-		rate: 1, // Start with a rate of 1.0
-		path: [ k ],  // Remember everywhere we have been
-	} );
-})
-//*/
-
-//* 
-// This is the a faster way to seed the queue
-// This will find the same cycles regardless of starting point
-// Can pick any point- 
-// here I choose a random name of a currency within the graph
-let first = Object.keys(graph)[Math.floor(Math.random() * Object.keys(graph).length)]
-
-queue.push( {
-	start: first, // Remember where we started
-	at: first,  // We are at where we start
-	rate: 1,  // start with a rate of 1.0
-	path: [first], // Remember everywhere we have been
-})
-//*/
 
 // Measure the score for a node in the same units it started in 
 // Transition it back to the starting point and get a score
@@ -98,28 +85,60 @@ function stepTo(data, place) {
 	// If this node is not already there:
 	if (data.at !== place) {
 		// Return the adjusted data
+		var nextRate = data.rate * graph[data.at][place];
 		return { // Object literal
 			// Make the transition to the destination place 
 			start: data.start, // Same starting location
 			at: place,			// Now at that place 
-			rate: data.rate * graph[data.at][place], // Apply rate change 
-			path: [ ... data.path, place ] // update path with new place 
+			rate: nextRate, // Apply rate change 
+			path: [ ... data.path, place ], // update path with new place 
 			// ... is the spread operator, it takes everything data.path,
 			// and 'spreads' those values in order in the new array.
+			// And this was an addition, score the nodes based off of the gains per trade.
+			score: (nextRate - 1.0) / (data.path.length)
 		}
 	}
 	// if we're already there, just return the data.
 	return data;
 }
+
+
+//* This will find all possible cycles, and score them properly.
+Object.keys(graph).forEach((k) => {
+	queue.put( { 
+		start: k, // Remember where we start
+		at: k, // current location is where we are starting initially
+		rate: 1, // Start with a rate of 1.0
+		score: 0, // No score because no trades/gains.
+		path: [ k ],  // Remember everywhere we have been
+	} );
+})
+//*/
+
+/* 
+// This will find the same 9-cycles regardless of starting point
+// Can pick any point- 
+// here I choose a random name of a currency within the graph
+let first = Object.keys(graph)[Math.floor(Math.random() * Object.keys(graph).length)]
+queue.put({
+	start: first, // Remember where we started
+	at: first,  // We are at where we start
+	rate: 1,  // start with a rate of 1.0
+	path: [first], // Remember everywhere we have been
+})
+//*/
 // Let's time how long it takes
 let startDate = new Date();
 console.log("Ready. Running.");
 
 // Continue while there are things in the queue
-while (queue.length > 0) {
+//while (queue.length > 0) {
+// Custom Queue version:
+while (queue.count() > 0) {
 	// Pop a data off the queue
-	let data = queue.pop();
-	
+	// let data = queue.pop();
+	// Custom Queue version:
+	let data = queue.take();
 	// Loop over all vertex names in our graph
 	Object.keys(graph).forEach((k) => {
 		// If we haven't been there yet...
@@ -139,7 +158,9 @@ while (queue.length > 0) {
 			// Ad the transitioned node back to the queue.
 			// unshift adds to opposite end of queue
 			// so pop() doesn't remove it immediately
-			queue.unshift(trace);
+			// queue.unshift(trace);
+			// Custom Queue version:
+			queue.put(trace);
 		} else {
 			// If we have been there, skip it.
 		}
@@ -152,11 +173,20 @@ let diff = endDate.getTime() - startDate.getTime();
 	
 console.log("\n\nDONE!");
 console.log("Took " + diff + "ms");	
+console.log("Found " + finished.length + " profitable cycles!");	
 
 // Sort the data.
 // Comparison function is the parameter
 finished.sort( (a,b) => { return b.rate - a.rate; } );
 
 // Print the best one!
-console.log("\n\nBest arbitrage:");
+console.log("\n\nBest Overall arbitrage:");
+console.log(finished[0]);
+
+
+finished.sort( (a,b) => { return b.score - a.score; } );
+
+
+// Print the actually best one!
+console.log("\n\nMost efficient arbitrage:");
 console.log(finished[0]);
