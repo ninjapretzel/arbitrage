97552354cdc2315f8323d03189733886a9787499
ninjapretzel <ninjapretzel@yahoo.com>
2019-12-03 18:25:44 -0700

    Revises C# program

---
+++ ExampleCsharpOutput.md
@@ -0,0 +1,21 @@
+# C#/Mono Example Output
+```λ mcs arbitrage.cs && mono arbitrage.exe
+Ready. Running.
+
+
+Finished in 31.9136ms
+
+
+Best Overall Arbitrage:
+Path: [Ruble -> Sweden -> Dollar -> Canada -> Euro -> Pound -> Yen -> Norway -> Australia -> Ruble]
+Rate: 1.24433215336135
+Score: 0.0271480170401495
+
+
+Most Efficient Arbitrage:
+Path: [Yen -> Norway -> Australia -> Yen]
+Rate: 1.12575
+Score: 0.0419166666666667
+
+Press Enter to continue...
+```
--- arbitrage.cs
+++ arbitrage.cs
@@ -9,36 +9,31 @@ using System.Threading.Tasks;
 
 namespace Arbitrage {
 	
+	/// <summary> Class that holds information about a trace state in the graph </summary>
 	class WorkData {
+		/// <summary> Starting node </summary>
 		public string start;
+		/// <summary> Current node </summary>
 		public string at;
+		/// <summary> Path from starting node to current node </summary>
 		public List<string> path;
+		/// <summary> Current rate </summary>
 		public double rate;
+		/// <summary> Score (rate - 1.0) / NumTrades </summary>
 		public double score;
 
-		public override string ToString() {
-			StringBuilder str = new StringBuilder();
-			str.Append("Start: ");
-			str.Append(start);
-			str.Append("\nPath: [");
-			foreach (var p in path) { 
-				str.Append(p);
-				str.Append(", ");
-			}
-			str.Append("]\nRate: ");
-			str.Append(rate);
-			str.Append("\nScore: ");
-			str.Append(score);
+		/// <inheritdoc />
+		public override string ToString() { return $"Path: [{string.Join(" -> ", path)}]\nRate: {rate}\nScore: {score}"; }
 
+		/// <summary> Creates a string for the output</summary>
+		/// <returns> String with the path and resulting rate multiplied by 1,000 </returns>
+		public string ShortString() { return $"Path: [ {string.Join(" -> ", path)} ] $1,000 => ${rate*1000.0:#,##0.00}"; }
 
-			return str.ToString();
-		}
-
-		public string ShortString() {
-			return $"Path: [ {string.Join(" -> ", path)} ] $1,000 => ${rate*1000.0:#,##0.00}";
-		}
 	}
+	/// <summary> Class to hold a graph. Names of every vertex in the graph will be a top-level key.
+	/// Top level values are themselves maps describing possible transitions from that vertex. </summary>
 	class Graph : Dictionary<string, Dictionary<string, double>> {
+
 		/// <summary> Transition function through the graph </summary>
 		/// <param name="data"> Current graph state to transition </param>
 		/// <param name="place"> New vertex to transition to </param>
@@ -61,39 +56,51 @@ namespace Arbitrage {
 				// Then add the place it is at now
 				next.path.Add(place);
 
+				// Return built node
 				return next;
 			}
 			// Already there? Return what we were given
 			return data;
 		}
+
 	}
 
+	/// <summary> Class holding entry point and helpers </summary>
 	class Program {
 
+		/// <summary> Nice little helper that gets the path to the source directory. </summary>
+		/// <param name="callerPath"> Autofilled by compiler due to <see cref="CallerFilePathAttribute"/> decoration. Full file path to sourcefile that calls the function. </param>
+		/// <returns> The directory name of the source file that calls it. </returns>
 		public static string SourceFileDirectory([CallerFilePath] string callerPath = "[NO PATH]") {
 			return callerPath.Substring(0, callerPath.Replace('\\', '/').LastIndexOf('/'));
 		}
 
+		/// <summary> Entry point </summary>
+		/// <param name="args"> Command line args</param>
 		static void Main(string[] args) {
-			Console.WriteLine(SourceFileDirectory());
+
+			// Read text from file 
 			string text = File.ReadAllText(SourceFileDirectory() + "/stuff.dat");
-			Console.WriteLine(text);
+			// Split into lines 
 			string[] lines = text.Split('\n');
 
+			// Create data structures
 			Graph graph = new Graph();
 			Queue<WorkData> queue = new Queue<WorkData>();
-			List<WorkData> finished = new List<WorkData>();
+			List<WorkData> profitable = new List<WorkData>();
 
-			// for (String line : lines) // java version
+			// Loop over lines in file 
 			foreach (string line in lines) {
-				// Split
+				// Split each line, and remove empty strings
 				string[] parts = line.Split(' ', '\t').Where(it => (it != "")).ToArray();
 
-				Console.WriteLine(parts.Length + " " + line);
+				// Skip lines that are not exactly 3 long
 				if (parts.Length != 3) { continue; }
+				// Extract data from line: [ from, to, rate ]
 				string from = parts[0];
 				string to = parts[1];
 				double rate;
+				// Skip lines that don't have a number in 3rd element
 				if (!double.TryParse(parts[2], out rate)) { continue; }
 
 				// Add new verticies if they don't already exist
@@ -108,15 +115,17 @@ namespace Arbitrage {
 				graph[from][to] = rate;
 				graph[to][from] = 1.0 / rate;
 			}
-			Console.WriteLine("Test");
-
+			// Loop over key/value pairs in graph
 			foreach (var pair in graph) {
+				/* // To print out graph (if we want)
 				Console.WriteLine(pair.Key + " : {");
 				foreach (var pair2 in pair.Value) {
 					Console.WriteLine("\t" + pair2.Key + " : " + pair2.Value);
 				}
 				Console.WriteLine("}");
+				//*/
 
+				// Create a new work data that starts at each vertex,
 				WorkData data = new WorkData();
 				data.start = pair.Key;
 				data.at = pair.Key;
@@ -125,29 +134,35 @@ namespace Arbitrage {
 				data.path = new List<string>();
 				data.path.Add(pair.Key);
 
+				// and add it to our queue
 				queue.Enqueue(data);
 			}
-
+			Console.WriteLine("Ready. Running.");
+			// Time how long it takes
 			DateTime start = DateTime.UtcNow;
+			// While queue has stuff in it...
 			while (queue.Count > 0) {
+				// Get front thing in queue
 				WorkData data = queue.Dequeue();
-				
-
+				// Score this data to see how good it is
 				WorkData compare = graph.Step(data, data.start);
-
+				
+				// Loop over graph:
 				foreach (var pair in graph) {
 
-					// If we haven't been there yet
+					// If we haven't been to that vertex yet
 					if (!data.path.Contains(pair.Key)) {
+						// Take transition to that location 
 						WorkData next = graph.Step(data, pair.Key);
+						// Score the new data to see how good it is 
 						WorkData final = graph.Step(next, data.start);
 
 						// See if it is profitable, then add it to the list of profitable trades
 						if (final.rate > 1) {
-							finished.Add(final);
+							profitable.Add(final);
 						}
 
-						// Ignore anywhere we lose money!
+						// Ignore anywhere we lose money, even if it's still profitable
 						if (final.rate >= compare.rate) {
 							queue.Enqueue(next);
 						}
@@ -157,32 +172,40 @@ namespace Arbitrage {
 				}
 
 			}
+			// Finished! Take the time
 			DateTime end = DateTime.UtcNow;
 			TimeSpan diff = end - start;
 
-			finished.Sort((a, b) => (a.rate - b.rate) < 0 ? -1 : 1 );
+			// Sort our data in Ascending order 
+			profitable.Sort((a, b) => (a.rate - b.rate) < 0 ? -1 : 1 );
+			// Effecient output building (lots of text to append, avoid using += )
 			StringBuilder output = new StringBuilder();
 
-			foreach (var profit in finished) {
-				Console.WriteLine(profit.ShortString());
+			// loop over all profitable paths
+			foreach (var profit in profitable) {
+				// Console.WriteLine(profit.ShortString());
 				output.Append(profit.ShortString());
 				output.Append('\n');
 			}
-
-
+			
 			Console.WriteLine("\n\nFinished in " + diff.TotalMilliseconds + "ms");
 			output.Append("\n\nFinished in " + diff.TotalMilliseconds + "ms");
 
-			finished.Sort((a, b) => (a.rate - b.rate) > 0 ? -1 : 1 );
-			Console.WriteLine("\n\nBest Overall Arbitrage: \n" + finished[0]);
-			output.Append("\n\nBest Overall Arbitrage: \n" + finished[0]);
+			// Sort our data in Decending order
+			profitable.Sort((a, b) => (a.rate - b.rate) > 0 ? -1 : 1 );
+			Console.WriteLine("\n\nBest Overall Arbitrage: \n" + profitable[0]);
+			output.Append("\n\nBest Overall Arbitrage: \n" + profitable[0]);
 
-			finished.Sort((a, b) => (a.score - b.score) > 0 ? -1 : 1 );
-			Console.WriteLine("\n\nMost Efficient Arbitrage: \n" + finished[0]);
-			output.Append("\n\nMost Efficient Arbitrage: \n" + finished[0]);
+			// Sort our data in Decending order by scores instead:
+			profitable.Sort((a, b) => (a.score - b.score) > 0 ? -1 : 1 );
+			Console.WriteLine("\n\nMost Efficient Arbitrage: \n" + profitable[0]);
+			output.Append("\n\nMost Efficient Arbitrage: \n" + profitable[0]);
 
+			// Dump output to file
 			File.WriteAllText(SourceFileDirectory()+"/output.txt", output.ToString());
 
+			// Pause console
+			Console.WriteLine("\nPress Enter to continue...");
 			Console.Read();
 		}
 	}
