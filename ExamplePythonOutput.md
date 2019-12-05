ed462d5a04a44e5c213b13667cf0ec0a81cc78ae
ninjapretzel <ninjapretzel@yahoo.com>
2019-12-04 22:23:59 -0700

    Revises python version + adds comments and sample output

---
+++ ExamplePythonOutput.md
@@ -0,0 +1,14 @@
+# Go Python Output
+```
+λ python arbitrage.py
+Wow that was slow, it took 63.86ms
+found 16358 profitable things
+
+
+Best overall path:
+{'at': 'Ruble', 'start': 'Ruble', 'rate': 1.2443321533613452, 'score': 0.027148017040149464, 'path': ['Ruble', 'Sweden', 'Dollar', 'Canada', 'Euro', 'Pound', 'Yen', 'Norway', 'Australia', 'Ruble']}
+
+
+Most Efficient path:
+{'at': 'Yen', 'start': 'Yen', 'rate': 1.12575, 'score': 0.04191666666666668, 'path': ['Yen', 'Norway', 'Australia', 'Yen']}
+```
--- arbitrage.py
+++ arbitrage.py
@@ -1,9 +1,9 @@
 import datetime
 
 # Open file and read all lines
-with open("stuff.dat", "r") as file:
+with open("stuff.dat", "r") as file: #{
 	lines = file.read().split('\n')
-	#data = file.read()
+#}
 
 # Create data structures for stuff
 # graph is a Dictionary of string: Dictionary
@@ -14,50 +14,74 @@ graph = { }
 queue = [ ]
 # And another for the ones that proiftable
 profitable = []
-for line in lines:
+
+for line in lines: #{
+	# Split line into segments separated by whitespace
 	parts = line.split()
 	
-	if len(parts) != 3:
+	# Skip any that aren't 3 long
+	if len(parts) != 3: #{ continue; }
 		continue
+	#} 
 	
 	currencyFrom = parts[0]
 	currencyTo = parts[1]
-	try:
+	# Skip any that don't have a number in the last place
+	try:#{ rate = float(parts[2]); } except: { continue; }
 		rate = float(parts[2])
-	except:
+	#}
+	except: #{
 		continue
+	#}
 	
-	if currencyFrom not in graph:
+	# If we don't have verticies in our graph, create them
+	if currencyFrom not in graph: #{ graph[currencyFrom] = {}; }
 		graph[currencyFrom] = {}
-	if currencyTo not in graph:
+	#}
+	if currencyTo not in graph: #{ graph[currencyTo] = {}; }
 		graph[currencyTo] = {}
+	#}
 	
+	# Add transitions, from -> to is rate, to -> from is reciprocal
 	graph[currencyFrom][currencyTo] = rate
 	graph[currencyTo][currencyFrom] = 1.0/rate
 	
-# end loading file
+#} # end loading file
+
 # print(graph)
 
-
-def step(data, place):
-	if data["at"] != place:
+# Transition function 
+def step(data, place): #{
+	# If data isn't at the place...
+	if data["at"] != place: #{
+		# Create a version of data transitioned there.
+		# Calculate rate
 		nextRate = data["rate"] * graph[data["at"]][place]
 		
 		return {
+			# Move to place 
 			"at": place,
+			# Same starting location 
 			"start": data["start"],
+			# Use calculated rate 
 			"rate": nextRate,
-			# relative gains per trade
+			# Score node based on relative gains per trade
 			# (rate - 1.0) / numTrades
 			"score": (nextRate - 1.0) / len(data["path"]),
+			# Add place to our path
 			"path": [ *data["path"], place ]
 		}
+	#}
+	# Otherwise, just return the data we built
 	return data
-def stepToStart(data):
+#} end step
+# Helper, transition a node back to it's starting location 
+def stepToStart(data): #{ return step(data, data["start"]); }
 	return step(data, data["start"])
+#} end stepToStart
 
-start = datetime.datetime.now()
-for k in graph:
+# Seed queue by starting at every single vert in the graph
+for k in graph: #{
 	data = {
 		"at": k,
 		"start": k,
@@ -66,42 +90,85 @@ for k in graph:
 		"path": [ k ]
 	}
 	queue.append(data)
+#} end seeding queue
 
 # print(queue)
 
-while (len(queue) > 0):
+# Time it because why not?
+start = datetime.datetime.now()
+# while there's work
+while (len(queue) > 0): #{
+	# Grab + remove next item from queue
 	current = queue[0]
 	queue.remove(current)
-	# 
+	
+	# Step that item back to the start for a normalized comparison
 	compare = stepToStart(current)
-	for k in graph:
-		if (k not in current["path"]):
+	
+	# Loop over all keys (verts) in graph
+	for k in graph: #{
+		# Skip repeating any places we've already been
+		if (k not in current["path"]): #{
+			# Step current to destination 
 			next = step(current, k)
+			# Step that back to start to normalize gains
 			scored = stepToStart(next)
 			
-			if (scored["rate"] > 1):
+			# If the thing is profitable, add it to our profitable list
+			if (scored["rate"] > 1): #{ profitable.append(scored); }
 				profitable.append(scored)
+			#}
 			
-			# Bellman ford, discard negative cash flow
+			# Bellman ford's part: discard negative cash flow
 			# by not traversing negative possibilities
-			if (scored["rate"] >= compare["rate"]):
+			if (scored["rate"] >= compare["rate"]): #{ queue.append(next); }
+				# Add more work to queue if it looks good
 				queue.append(next)
-end = datetime.datetime.now()
-diff = end - start
-print("It only took " + str(diff) + "s")
-print("found " + str(len(profitable)) + " profitable things")
+			#}
+		#}
+	#} End graph lop
+#} End work loop
 
-def byRate(a):
+# Take end time
+end = datetime.datetime.now()
+# Find how long it took
+diff = end - start
+# Open output file
+fout = open("outputpy.txt", "w")
+
+# sorting (unfortunately no standardized lambda sort)
+def byRate(a): #{ return a.rate; }
 	return a["rate"]
 			
-def byScore(a):
+def byScore(a): #{ return a.score; }
 	return a["score"]
-	
-print("\n\n\nBest overall path:")
+
+# Sort Ascending by rate for file printout
+sortedbyrate = sorted(profitable, key=byRate, reverse=False)
+i = 0
+for data in sortedbyrate: #{
+	i += 1
+	fout.write("Path " + str(i) + ": " + (" => ".join(data["path"])) + ", $1000.00 =>" + str(data["rate"] * 1000.0)+"\n")
+#}
+
+# Be cheeky about how long it took
+print("Wow that was slow, it took " + str(diff.total_seconds() * 1000.0) + "ms")
+fout.write("Wow that was slow, it took " + str(diff.total_seconds() * 1000.0) + "ms\n")
+print("found " + str(len(profitable)) + " profitable things")
+fout.write("found " + str(len(profitable)) + " profitable things\n")
+
+# Print out highest return rate after trading with no revisits
+fout.write("\n\nBest overall path:\n")
+print("\n\nBest overall path:")
 sortedbyrate = sorted(profitable, key=byRate, reverse=True)
+fout.write(str(sortedbyrate[0]) + "\n")
 print(sortedbyrate[0])
 
-print("\n\n\nMost Efficient path:")
+# Print out most efficient tight loop
+fout.write("\n\nMost Efficient path:\n")
+print("\n\nMost Efficient path:")
 sortedbyscore = sorted(profitable, key=byScore, reverse=True)
+fout.write(str(sortedbyscore[0]) + "\n")
 print(sortedbyscore[0])
 
+fout.close()
